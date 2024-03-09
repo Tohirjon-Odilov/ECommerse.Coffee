1fc67517480647aa2f140e058a140c8bad3ce3c2
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-09 16:03:07 +0500

    Category Configuratin fill

---
+++ ECommerse.Coffee.Infrastructure/Persistance/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using ECommerse.Coffee.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerse.Coffee.Infrastructure.Persistance.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.HasData(new Category()
+            {
+                Id = 1,
+                Name = "Sutli Coffee"
+            });
+        }
+    }
+}
