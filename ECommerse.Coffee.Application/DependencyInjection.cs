b72e15225bd183655c7b2e279895615f0fccbaa5
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-03-09 15:45:00 +0500

    add dependency injection

---
+++ ECommerse.Coffee.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.Extensions.DependencyInjection;
+
+//namespace ECommerse.Coffee.Application
+//{
+//    public static class DependencyInjection
+//    {
+//        public static IServiceCollection AddApplication(this WebApplication services)
+//        {
+//            //services.AddScoped<IUserService, UserService>();
+//            //services.AddScoped<IAuthService, AuthService>();
+//            //services.AddScoped<IEmailSenderService, EmailSenderService>();
+//            //services.AddScoped<ICategoryService, CategoryService>();
+//            //services.AddScoped<IProductService, ProductService>();
+//            //services.AddScoped<IStatsService, StatsService>();
+
+
+//            return services;
+//        }
+//    }
+//}
