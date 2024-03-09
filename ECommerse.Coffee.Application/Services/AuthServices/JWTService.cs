5f07d1e2d5a8a7c3efcf15ac54ed5da1b2d8c4c6
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-03-09 12:14:08 +0500

    delete wetherforecast and add jwt some files

---
+++ ECommerse.Coffee.Application/Services/AuthServices/JWTService.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace ECommerse.Coffee.Application.Services.AuthServices.AuthServices
+{
+    public class JWTService
+    {
+        public static TokenValidationParameters GetTokenValidationParameters(IConfiguration configuration)
+        {
+            return new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = configuration["JWT:ValidIssuer"],
+                ValidAudience = configuration["JWT:ValidAudience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"])),
+                ClockSkew = TimeSpan.Zero,
+            };
+        }
+    }
+}
