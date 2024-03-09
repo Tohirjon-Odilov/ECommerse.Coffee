839c103c4d709366cab5199517a006d18b6535b5
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-09 15:10:23 +0500

    Admin configuration is filled

--- ECommerse.Coffee.Domain/Entities/Product.cs
+++ ECommerse.Coffee.Domain/Entities/Product.cs
@@ -11,8 +11,9 @@ namespace ECommerse.Coffee.Domain.Entities
     public class Product : BaseEntity
     {
         public string Name { get; set; }
-        public int CategoryId {  get; set; }
+        public int CategoryId { get; set; }
         public decimal Price { get; set; }
+        public string PicturePath { get; set; }
         public string Description { get; set; }
         [ForeignKey(nameof(CategoryId))]
         public virtual Category Category { get; set; }
---
+++ ECommerse.Coffee.Infrastructure/Persistance/Configurations/AdminConfigurations.cs
@@ -0,0 +1,55 @@
+﻿using ECommerse.Coffee.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerse.Coffee.Infrastructure.Persistance.Configurations
+{
+    public class AdminConfigurations : IEntityTypeConfiguration<Admin>
+    {
+        public void Configure(EntityTypeBuilder<Admin> builder)
+        {
+            builder.Property("FirstName")
+                .HasMaxLength(25)
+                .IsRequired();
+
+            builder.Property("LastName")
+                .HasMaxLength(25)
+                .IsRequired();
+
+            builder.Property("PhoneNumber")
+                .HasMaxLength(15)
+                .IsRequired();
+
+
+            builder.HasData(new Admin()
+            {
+                Id = 1,
+                FirstName = "Ibrohim",
+                LastName = "Qosimov",
+                Email = "ibrohim.qosiomov@gmail.com",
+                PhoneNumber = "+9989843943489",
+            });
+            builder.HasData(new Admin()
+            {
+                Id = 2,
+                FirstName = "Tohirjon",
+                LastName = "Odilov",
+                Email = "tohirjon-odilov@outlook.com",
+                PhoneNumber = "+998999999999"
+            });
+            builder.HasData(new Admin()
+            {
+                Id = 3,
+                FirstName = "Akramjon",
+                LastName = "Abduvahobov",
+                Email = "akramzonabduvahobov6@gmail.com",
+                PhoneNumber = "+998997777777"
+            });
+        }
+    }
+}
